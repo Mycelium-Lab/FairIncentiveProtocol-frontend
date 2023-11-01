891791d71d08541dd38765489cc0af0207fbab2f
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2023-11-01 15:14:25 +0500

    fix overlay of modal windows

--- src/components/common/modals/progress.js
+++ src/components/common/modals/progress.js
@@ -7,10 +7,33 @@ class ProgressModal extends Component {
         super(props)
     }
 
+     entering() {
+        const mint = document.getElementById('mint')
+        const createUser = document.getElementById('createUser')
+        const editUser = document.getElementById('editUser')
+        const modal = mint?.parentElement || createUser?.parentElement || editUser?.parentElement
+        if(modal) {
+            modal.style.zIndex = '1049'
+        }
+    }
+
+    entered() {
+        const mint = document.getElementById('mint')
+        const createUser = document.getElementById('createUser')
+        const editUser = document.getElementById('editUser')
+        const modal = mint?.parentElement || createUser?.parentElement || editUser?.parentElement
+        if(modal) {
+            modal.style.zIndex = '1050'
+        }
+    }
+
+    entering = this.entering.bind(this)
+    entered = this.entered.bind(this)
+
     render() {
-        return <Modal show={this.props.showProgress} onHide={this.props.handleCloseProgress} centered>
+        return <Modal onEntering={this.entering} onEntered={this.entered} show={this.props.showProgress} onHide={this.props.handleCloseProgress} centered>
              <Modal.Header  className="modal-newuser__title modal-title" closeButton>
-                 Purchace
+                {this.props.progressTitle ? this.props.progressTitle : 'Progress'}
             </Modal.Header>
             <Modal.Body>
                 <div className="confirm__body">
