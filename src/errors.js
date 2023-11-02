da08225623d132127dfff6f235ed78a3328875a3
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2023-11-02 08:26:58 +0500

    handle registration errors

--- src/errors.js
+++ src/errors.js
@@ -1,4 +1,6 @@
 export default {
       "<email> must be a valid email": "Email is invalid",
-     "<wallet> is incorrect": 'The wallet address is incorrect'
+     "<wallet> is incorrect": 'The wallet address is incorrect',
+     "This <email> already exist": 'This email already exists',
+     "<repeat_password> must be [ref:password]": "Password mismatch" 
   }
