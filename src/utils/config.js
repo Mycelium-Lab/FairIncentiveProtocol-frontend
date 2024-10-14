073b88b8223937fec00b8d8505a1102b470f759e
elolpuer <egorgoncharov6755@gmail.com>
2024-10-14 12:58:08 +0700

    update api address

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export const config = {
     projectIdWalletConnect: '1a69f3b6a58d013ce257eb4c6f6f8d97',
-    api: 'https://kongam.space',
+    api: 'https://chippolino.space',
     // api: 'http://localhost:3000',
     signerAddress: '0x38aE865Add0b9715b6cfb6e5AA03810715078f9A',
     status: "test" //prod
